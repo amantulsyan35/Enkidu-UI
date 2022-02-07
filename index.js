0996c16d826fd519371ac8e02eb8eff540628964
Aman Tulsyan <65289994+amantulsyan35@users.noreply.github.com>
2022-02-07 15:32:43 +0530

    feat:add sidebar links

---
+++ index.js
@@ -0,0 +1,6 @@
+// const navAvatar = document.querySelector('#nav-avatar');
+// const avatarId = document.querySelector('#avatar-id');
+
+// navAvatar.addEventListener('click', function () {
+//   window.location.href = '#avatar-id';
+// });
