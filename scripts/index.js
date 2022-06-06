d5829434d1692ee0dafae61ecb571f6b33d94282
Aman Tulsyan <65289994+amantulsyan35@users.noreply.github.com>
2022-06-06 18:02:06 +0530

    update links

--- scripts/index.js
+++
@@ -1,6 +0,0 @@
-// const navAvatar = document.querySelector('#nav-avatar');
-// const avatarId = document.querySelector('#avatar-id');
-
-// navAvatar.addEventListener('click', function () {
-//   window.location.href = '#avatar-id';
-// });
